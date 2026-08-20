1ffbf8a2cd1f9011462d3882d1d8e20510697fc1
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-08-21 02:22:35 +0300

    Add project files.

---
+++ Assignment01Advanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment01Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
