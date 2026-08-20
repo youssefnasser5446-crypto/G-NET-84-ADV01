d2dfe01d90768857b30fd15b4fa9238891e43aab
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-08-21 02:59:50 +0300

    Question02

--- Assignment01Advanced/Program.cs
+++ Assignment01Advanced/Program.cs
@@ -15,6 +15,21 @@
 
              */
             #endregion
+
+            #region Question02
+
+            ///*
+            // Q2: Write a generic class Container<T> with Add and Get methods.
+            //*/
+            //Containers<int> co = new Containers<int>();
+            //co.Add(1);
+            //co.Add(2);
+            //co.Add(3);
+            //Console.WriteLine($"index 1 is : {co.Get(1)}");//2
+            //Console.WriteLine(co);//2
+
+            #endregion
+
         }
     }
 }
